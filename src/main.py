22b1713af407fb644a34b3ba70641684a214367b
mariantomuh <35054527+mariantomuh@users.noreply.github.com>
2025-09-25 09:59:24 +0700

    Update main.py
    merubah isi main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,1 @@
-# rencana proyek
-5. - Tujuan: Membuat aplikasi To Do List berbasis Python
-6. - Tools: Python, Tkinter
-7. - Fitur Utama: tambah tugas, hapus tugas, lihat daftar tugas
+#isinya saya rubah
